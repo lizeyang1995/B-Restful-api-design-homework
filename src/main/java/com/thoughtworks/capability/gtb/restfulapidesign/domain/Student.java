25f117f436387b1c42c66cff5be500f39fb5641c
lizeyang1995 <424011527@qq.com>
2020-11-05 23:59:06 +0800

    [lizeyang] feat: create a class named Student

---
+++ src/main/java/com/thoughtworks/capability/gtb/restfulapidesign/domain/Student.java
@@ -0,0 +1,17 @@
+package com.thoughtworks.capability.gtb.restfulapidesign.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+    private Integer id;
+    private String name;
+    private String gender;
+    private String note;
+}
