98a7985ad38f561181b5c3b0abef391aaccc3bbb
lizeyang1995 <424011527@qq.com>
2020-11-06 00:48:39 +0800

    [lizeyang] feat: add a test, should delete student success

--- src/test/java/com/thoughtworks/capability/gtb/restfulapidesign/controller/StudentControllerTest.java
+++ src/test/java/com/thoughtworks/capability/gtb/restfulapidesign/controller/StudentControllerTest.java
@@ -3,27 +3,40 @@ package com.thoughtworks.capability.gtb.restfulapidesign.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.thoughtworks.capability.gtb.restfulapidesign.domain.Student;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class StudentControllerTest {
     @Autowired
     MockMvc mockMvc;
     @Test
+    @Order(1)
     void should_add_a_student_success() throws Exception {
         Student student = Student.builder().name("lizeyang").gender("male").note("123").build();
         ObjectMapper objectMapper = new ObjectMapper();
         String jsonStudent = objectMapper.writeValueAsString(student);
         mockMvc.perform(post("/students").content(jsonStudent).contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isCreated());
+    }
+
+    @Test
+    @Order(2)
+    void should_delete_student_success() throws Exception {
+        mockMvc.perform(delete("/students/1"))
+                .andExpect(status().isOk());
     }
 }
