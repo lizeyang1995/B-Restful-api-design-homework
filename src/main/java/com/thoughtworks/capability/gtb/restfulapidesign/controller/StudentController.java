08093913842eca0680569677d66c05b788809ecb
lizeyang1995 <424011527@qq.com>
2020-11-06 00:22:35 +0800

    [lizeyang] feat: complete a test, should add a student success

--- src/main/java/com/thoughtworks/capability/gtb/restfulapidesign/controller/StudentController.java
+++ src/main/java/com/thoughtworks/capability/gtb/restfulapidesign/controller/StudentController.java
@@ -1,6 +1,7 @@
 package com.thoughtworks.capability.gtb.restfulapidesign.controller;
 
 import com.thoughtworks.capability.gtb.restfulapidesign.domain.Student;
+import com.thoughtworks.capability.gtb.restfulapidesign.service.StudentService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,9 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StudentController {
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
     @PostMapping("students")
     @ResponseStatus(HttpStatus.CREATED)
     public Student addStudent(@RequestBody Student student) {
+        studentService.addStudent(student);
         return student;
     }
 }
